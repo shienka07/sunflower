f3ab3aa4a4f512f7168a2ce55fc0ca6df0e076d4
qus0in <qus0in@gmail.com>
2025-06-12 10:29:13 +0900

    REST API 준비

--- docs/main.js
+++ docs/main.js
@@ -1,36 +1,8 @@
-const apiKey = document.querySelector('#prompt-key');
-const promptInput = document.querySelector('#prompt-input');
-
-const form = document.querySelector('#prompt-form');
-const result = document.querySelector('#result');
-
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  alert(`프롬프트 전송! API 키: ${apiKey.value}, 프롬프트: ${promptInput.value}`);
-  const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=' + apiKey.value, {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-        contents: [
-            {
-            parts: [
-                {
-                text: "뒤에 입력한 프롬프트 초안을 PTCF(Persona, Task, Context, Format) 공식에 따라 보강. 프롬프트 초안 : {" + promptInput.value + "}",
-                },
-            ],
-            },
-        ],
-        }),
-    });
-    if (response.ok) {
-        const data = await response.json();
-        alert('응답: ' + JSON.stringify(data));
-        // result.textContent = JSON.stringify(data);
-        result.innerHTML = data.candidates[0].content.parts[0].text;
-    }
-    else {
-        alert('오류 발생: ' + response.statusText);
-    }
+document.getElementById('copyBtn').addEventListener('click', () => {
+    const input = document.getElementById('shareLink');
+    input.select();
+    document.execCommand('copy');
+    document.getSelection().removeAllRanges();
+    document.getElementById('copyBtn').textContent = '복사됨';
+    setTimeout(() => document.getElementById('copyBtn').textContent = '복사', 2000);
 });
