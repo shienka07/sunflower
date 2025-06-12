d138be80bd53b8a5fd6d8df9feb4c4707bd3b648
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-12 11:34:46 +0900

    수정

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,11 @@
-document.getElementById('copyBtn').addEventListener('click', () => {
-    const input = document.getElementById('shareLink');
-    input.select();
-    document.execCommand('copy');
-    document.getSelection().removeAllRanges();
-    document.getElementById('copyBtn').textContent = '복사됨';
-    setTimeout(() => document.getElementById('copyBtn').textContent = '복사', 2000);
+document.getElementById("copyBtn").addEventListener("click", () => {
+  const input = document.getElementById("shareLink");
+  input.select();
+  document.execCommand("copy");
+  document.getSelection().removeAllRanges();
+  document.getElementById("copyBtn").textContent = "복사됨";
+  setTimeout(
+    () => (document.getElementById("copyBtn").textContent = "복사"),
+    2000
+  );
 });
